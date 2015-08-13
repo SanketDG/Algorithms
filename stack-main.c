1549d0185b4524f6ebc2448ae55b67234875b5bd
SanketDG <sanketdasgupta@gmail.com>
2015-08-13 23:58:16 +0530

    make menu program of stack separate

---
+++ stack-main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+int main(void)
+{
+    Stack* stack = createStack(100);
+
+    int n, x;
+
+    printf("===STACK PROGRAM===\n");
+
+    while(1)
+    {
+        printf("\n\nOperations:\n1. Push\n2. Pop\n3. Display\n4.Exit\nEnter operation: ");
+        scanf("%d", &x);
+
+        switch(x) {
+            case 1: {
+                printf("\nEnter element to push: ");
+                scanf("%d", &n);
+                push(stack, n);
+                break;
+            }
+            case 2: {
+                pop(stack);
+                break;
+            }
+            case 3: {
+                printf("\n\nStack --> ");
+                display(stack);
+                break;
+            }
+            case 4: {
+                exit(0);
+                break;
+            }
+            default : printf("Invalid Input\n");
+        }
+    }
+
+    return 0;
+}
--- stack.c
+++ stack.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 #include "stack.h"
 
 Stack* createStack(int capacity)
@@ -46,42 +45,4 @@ void display(Stack* stack)
         printf("%d ",stack->array[i]);
     }
     printf("\n");
-}
-
-int main(void)
-{
-    Stack* stack = createStack(100);
-
-    int n, x;
-
-    printf("===STACK PROGRAM===\n");
-
-    while(1)
-    {
-        printf("\n\nOperations:\n1. Push\n2. Pop\n3. Display\n4.Exit\nEnter operation: ");
-        scanf("%d", &x);
-
-        switch(x) {
-            case 1: {
-                printf("\nEnter element to push: ");
-                scanf("%d", &n);
-                push(stack, n);
-                break;
-            }
-            case 2: {
-                pop(stack);
-                break;
-            }
-            case 3: {
-                printf("\n\nStack --> ");
-                display(stack);
-                break;
-            }
-            case 4: {
-                exit(0);
-                break;
-            }
-            default : printf("Invalid Input\n");
-        }
-    }
 }
